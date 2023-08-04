d271523055e6b994abd436eaf6672244eb569410
Krushna <okz2kor@bosch.com>
2023-08-04 04:46:56 +0000

    java modified

--- FirstJava.java
+++ FirstJava.java
@@ -2,6 +2,6 @@ public class FirstJava
 {
 	public static void main (String[] args)
 	{
-	System.out.println("Hello! welcome to Jenkins!");
+	System.out.println("Hello! welcome to Jenkins!Demonstrtating freestyle job");
 	}
 }
